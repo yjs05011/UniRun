11e63a22ad1af9c43799d077c38170c15f7e322d
ANJAEUK <yjs05011@naver.com>
2023-01-25 11:30:30 +0900

    Font Korea add

---
+++ Assets/Scripts/UI_HpBar.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_HpBar : MonoBehaviour
+{
+
+    public Slider hpbar = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // hpbar = GetComponent<Slider>();
+        // state = GameManager.Instance.StateCall(); //캐릭터 hp를 받아옴
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void PlayerHPbar()
+
+    {
+
+
+
+        
+
+    }
+}
+
